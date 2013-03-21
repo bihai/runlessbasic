9a5623bfa50596be60e2a22d2c34990807ca1a2c
Josh Hawcroft <dev@joshhawcroft.com>
2013-03-22 10:59:02 +1100

    added separate test and command-line tool profiles to the xcode project and sources

---
+++ Compiler/run-tool.c
@@ -0,0 +1,19 @@
+/*
+ * parser.h
+ * RunlessBASIC Compiler
+ *
+ * (c) 2013 Joshua Hawcroft
+ *
+ */
+
+#include <stdio.h>
+
+#include "parser.h"
+
+
+int main(int argc, const char * argv[])
+{
+    printf("RunlessBASIC Tool\n");
+    
+    return 0;
+}
